580ddc7f1993e27849e5dda7d05b5004a3e193c9
smiley <codemasher@users.noreply.github.com>
2023-10-13 15:44:04 +0200

    :octocat: Battle.net enrollment test disabled (#6)

--- tests/Authenticators/BattleNetTest.php
+++ tests/Authenticators/BattleNetTest.php
@@ -70,10 +70,18 @@ class BattleNetTest extends AuthenticatorInterfaceTestAbstract{
 		$this::markTestSkipped('N/A');
 	}
 
+	public function testGetServertime():void{
+		/** @noinspection PhpUnitTestFailedLineInspection */
+		$this::markTestSkipped('enrollment URL changed/disabled');
+	}
+
 	/**
 	 * @see https://github.com/winauth/winauth/blob/c57132f57b8a90e5219c628deb591f4603f27cb0/Authenticator/BattleNetAuthenticator.cs#L450-L451
 	 */
 	public function testRestoreSecret():void{
+		/** @noinspection PhpUnitTestFailedLineInspection */
+		$this::markTestSkipped('enrollment URL changed/disabled');
+
 		$serial       = 'US-1306-2525-4376';
 		$restore_code = 'CR24KPKF51';
 
