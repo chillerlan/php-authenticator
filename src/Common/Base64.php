8af3771fbb85284c2d940fe65a015e038cd2b743
smiley <codemasher@users.noreply.github.com>
2025-11-02 22:07:39 +0100

    :octocat: +EncoderInterface

--- src/Common/Base64.php
+++ src/Common/Base64.php
@@ -14,55 +14,49 @@ namespace chillerlan\Authenticator\Common;
 use InvalidArgumentException;
 use ParagonIE\ConstantTime\Base64 as ConstantTimeBase64;
 use SensitiveParameter;
-use function function_exists;
-use function preg_match;
+use function function_exists, preg_match;
 
 /**
  * Class to provide base64 encoding/decoding of strings using constant time functions
  */
-class Base64{
+class Base64 implements EncoderInterface{
 
 	/**
 	 * The Base64 character set as defined by RFC3548
 	 *
 	 * @see https://datatracker.ietf.org/doc/html/rfc3548#section-3
 	 * @see https://datatracker.ietf.org/doc/html/rfc4648#section-4
-	 *
-	 * @var string
 	 */
 	public const CHARSET = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=';
 
 	/**
 	 * Encode a string to Base64
 	 */
-	public static function encode(#[SensitiveParameter] string $str):string{
+	public static function encode(#[SensitiveParameter] string $string):string{
 
 		if(function_exists('sodium_bin2base64')){
-			return \sodium_bin2base64($str, \SODIUM_BASE64_VARIANT_ORIGINAL);
+			return \sodium_bin2base64($string, \SODIUM_BASE64_VARIANT_ORIGINAL);
 		}
 
-		return ConstantTimeBase64::encode($str);
+		return ConstantTimeBase64::encode($string); // @codeCoverageIgnore
 	}
 
 	/**
 	 * Decode a string from Base64
 	 */
-	public static function decode(#[SensitiveParameter] string $base64):string{
-		self::checkCharacterSet($base64);
+	public static function decode(#[SensitiveParameter] string $encodedString):string{
+		self::checkCharacterSet($encodedString);
 
 		if(function_exists('sodium_base642bin')){
-			return \sodium_base642bin($base64, \SODIUM_BASE64_VARIANT_ORIGINAL);
+			return \sodium_base642bin($encodedString, \SODIUM_BASE64_VARIANT_ORIGINAL);
 		}
 
-		return ConstantTimeBase64::decode($base64);
+		return ConstantTimeBase64::decode($encodedString); // @codeCoverageIgnore
 	}
 
-	/**
-	 * @throws \InvalidArgumentException
-	 */
-	public static function checkCharacterSet(#[SensitiveParameter] string $base64):void{
+	public static function checkCharacterSet(#[SensitiveParameter] string $encodedString):void{
 
-		if(!preg_match('#^[a-z\d/=+]+$#i', $base64)){
+		if(!preg_match('#^[a-z\d/=+]+$#i', $encodedString)){
 			throw new InvalidArgumentException('Base64 must match RFC4648 character set');
 		}
 
---
+++ src/Common/EncoderInterface.php
@@ -0,0 +1,45 @@
+<?php
+/**
+ * Interface EncoderInterface
+ *
+ * @created      02.11.2025
+ * @author       smiley <smiley@chillerlan.net>
+ * @copyright    2025 smiley
+ * @license      MIT
+ */
+declare(strict_types=1);
+
+namespace chillerlan\Authenticator\Common;
+
+use SensitiveParameter;
+
+/**
+ *
+ */
+interface EncoderInterface{
+
+	/**
+	 * The allowed character set
+	 *
+	 * @var string
+	 */
+	public const CHARSET = '';
+
+	/**
+	 * Encode a string
+	 */
+	public static function encode(#[SensitiveParameter] string $string):string;
+
+	/**
+	 * Decode a string
+	 */
+	public static function decode(#[SensitiveParameter] string $encodedString):string;
+
+	/**
+	 * Checks if the given string only contains allowed characters
+	 *
+	 * @throws \InvalidArgumentException
+	 */
+	public static function checkCharacterSet(#[SensitiveParameter] string $encodedString):void;
+
+}
